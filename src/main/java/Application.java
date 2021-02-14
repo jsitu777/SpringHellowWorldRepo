8a80e6058d503aff341c7fcf06cb89cf747471e2
wenjinsitu <59303945+wenjinsitu@users.noreply.github.com>
2021-02-14 14:24:32 -0800

    Add files via upload

---
+++ src/main/java/Application.java
@@ -0,0 +1,13 @@
+import com.leapcode.service.SpeakerService;
+import com.leapcode.service.SpeakerServiceImpl;
+
+// add some test comments here.
+// Test comments...
+//jim
+public class Application {
+    public static void main(String[] args){
+        SpeakerService service = new SpeakerServiceImpl();
+        System.out.println(service.findAll().get(0).getFirstName());
+        System.out.println(service.findAll().get(0).getLastName());
+    }
+}
